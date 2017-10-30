c5250053494a3195d0f21b40c886e46d9392fde1
Alvin Kong <alvink92@gmail.com>
2017-10-30 13:38:44 -0700

    finish v big sum

---
+++ lib/very_big_sum.rb
@@ -0,0 +1,12 @@
+#!/bin/ruby
+
+def aVeryBigSum(n, ar)
+    # Complete this function
+    ar.inject(:+)
+end
+
+n = gets.strip.to_i
+ar = gets.strip
+ar = ar.split(' ').map(&:to_i)
+result = aVeryBigSum(n, ar)
+puts result;
